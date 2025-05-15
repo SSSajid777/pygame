ad8fb60e6a250893497a99c290ecd71033b92d48
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-05-15 13:47:01 -0400

    Update Intro to Pygame.py

--- Intro to Pygame.py
+++ Intro to Pygame.py
@@ -9,6 +9,12 @@ screen = pygame.display.set_mode((width, height))
 #Main running loop
 running=True
 while running:
+    #Create an event in pygame
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        
+
     pygame.display.flip() #Refreshes the screen
 
 pygame.quit() #Quits pygame properly
