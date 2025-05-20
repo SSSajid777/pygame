db7febe0eca0aebb0c313e5ca2f972ba2d92d1ef
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-05-20 13:44:40 -0400

    Create pygameshapes.py

---
+++ pygameshapes.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+width, height = 400, 400
+screen = pygame.display.set_mode((width, height))
+
+rect_colour= 10, 120, 120
+
+running=True
+while running:
+
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+    pygame.draw.rect(screen, rect_colour, pygame.Rect(30, 30, 60, 60))
+
+    pygame.display.flip()
+
+pygame.quit()
+
