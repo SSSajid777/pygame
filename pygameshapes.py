6badfe68545a27c8d1959ea71ce2e8fce476cb4f
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-05-20 13:52:01 -0400

    Update pygameshapes.py

--- pygameshapes.py
+++ pygameshapes.py
@@ -3,19 +3,19 @@ import pygame
 pygame.init()
 width, height = 400, 400
 screen = pygame.display.set_mode((width, height))
-
+#Define a rectangle colour
 rect_colour= 10, 120, 120
-
+#Main running loop
 running=True
 while running:
-
+#Create an event in game
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
     pygame.draw.rect(screen, rect_colour, pygame.Rect(30, 30, 60, 60))
     pygame.draw.circle(screen, (200,0,0), (60,60),(10))
 
-    pygame.display.flip()
+    pygame.display.flip() #Refreshes the screen
 
-pygame.quit()
+pygame.quit() #Quits pygame properly
 
