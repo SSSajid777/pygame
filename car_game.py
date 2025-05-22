bf6c293cfd7a7e52d8cc656969121ce2e019ae7b
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-05-22 14:07:47 -0400

    Update car_game.py

--- car_game.py
+++ car_game.py
@@ -7,6 +7,11 @@ from car import Car        #How do we impoprt class (exam question)
 width, height= 400, 400
 screen = pygame.display.set_mode((width, height))
 
+GREEN=(100,255,200)
+GREY=(210,210,210)
+WHITE=(255,255,255)
+RED=(255,0,0)
+PURPLE=(255,0,255)
 
 running=True
 while running:
