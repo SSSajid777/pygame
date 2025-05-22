2b1f88b890dd94ef8d3a27da6c4ea84b21187c51
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-05-22 14:03:25 -0400

    Create car_game.py

---
+++ car_game.py
@@ -0,0 +1,18 @@
+import pygame 
+pygame.init() #Initialize a pygame class
+
+#Set our screen size
+width, height= 400, 400
+screen = pygame.display.set_mode((width, height))
+
+
+running=True
+while running:
+    #Create an event in pygame
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+
+    pygame.display.flip()
+
+pygame.quit() #Quits pygame properly
