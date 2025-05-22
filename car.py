8ecb8ce02176a2264e4de3265b31eea9db1a0077
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-05-22 13:58:19 -0400

    Update car.py

--- car.py
+++ car.py
@@ -2,11 +2,16 @@ import pygame
 
 class Car(pygame.sprite.Sprite):   #init is called constructor (exam question)
 
-
     def __init__(self, colour, width, height):
         #Call Sprite constructor
         super().__init__
 
-        self.image=pygame.surface((width, height))
-        self.image.fill(255,255,255)   
+        #Set the background colour and set it to be transparent
+        self.image=pygame.surface((width, height))  #Create out pygame surface
+        self.image.fill(255,255,255) #Fill the surface with a colour
+        self.image.set_colourkey(255,255,255) 
+
+        #Draw a rectangle
+        pygame.draw.rect(self.image, colour, [0,0, width, height])
+ 
      
