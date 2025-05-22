3fe7d884d35c8ef56a32387eafcbf43ac465ebc2
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-05-22 13:55:20 -0400

    Update car.py

--- car.py
+++ car.py
@@ -1,3 +1,12 @@
 import pygame
 
-class Car(pygame.sprite.Sprite):
+class Car(pygame.sprite.Sprite):   #init is called constructor (exam question)
+
+
+    def __init__(self, colour, width, height):
+        #Call Sprite constructor
+        super().__init__
+
+        self.image=pygame.surface((width, height))
+        self.image.fill(255,255,255)   
+     
