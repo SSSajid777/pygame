879a70a1af05a9423de2a70410726b4a7e728beb
SSSajid777 <sajidihsanabdullah@gmail.com>
2025-05-29 14:19:41 -0400

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -20,7 +20,7 @@ screen = pygame.display.set_mode((width, height))
 pygame.mixer.init()
 munch = pygame.mixer.Sound('munch.wav')
 game_background_music=pygame.mixer.Sound('game_background_sound.mp3')
-game_over_sound=pygame.mixer.Sound('game_over.mp3')
+game_over_sound=pygame.mixer.music.load(('game_background_sound'))
 
 #Snake Information
 snake_position = [360, 240]
@@ -75,7 +75,7 @@ def scoring(score):
 
     pygame.display.flip() #Update the screen
 
-game_background_music.play()
+game_background_music.play(-1)
 running = True
 while running:
     for event in pygame.event.get():
